c07a7a88e92da159865e460f7fdc0cbe6a67178a
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2025-10-26 15:26:17 +0530

    created signup.ts

--- backend/routes/crowd.ts
+++ backend/routes/crowd.ts
@@ -5,7 +5,7 @@ app.get("/crowd/info", (req, res) => {
   res.json({ location: "Nashik Kumbh Mela", crowdCount: 1000 });
 });
 
-app.post("/crowd/add", (req, res) => {
+app.post("/crowd/add", (req, res)=> {
   const { location, count } = req.body;
   res.json({ message: `Crowd data added for ${location}`, total: count });
 });
