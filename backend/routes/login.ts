84376af7cd03efcbdf61edb4d9f6cedd3720241a
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-10-26 15:38:31 +0530

    Fix: login.ts

--- backend/routes/login.ts
+++ backend/routes/login.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { loginUserSchema } from "../schemas/loginUserSchema";
+import { UserModel } from "../models/User";
 const app = express();
 
-app.get("/login", (req, res) => {
-  res.send("This is Login Route");
-});
-app.post("/login", (req, res) => {
-  res.send("This is Login route");
-});
-
-export default app;
+export const loginApp = async (req: Request, res: Response) => {
+  try {
+    const data = loginUserSchema.parse(req.body);
+    const { email, password } = data;
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return Response.json({ message: "Invalid email" }, { status: 401 });
+    }
+    const pass = await bcrypt.compare(password, user.password);
+    if (!pass) {
+      return Response.json({ message: "Invalid Password" }, { status: 401 });
+    }
+  } catch (error: any) {
+    return Response.json({ message: error.message }, { status: 500 });
+  }
+};
